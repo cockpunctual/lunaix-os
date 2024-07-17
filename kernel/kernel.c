5877ebd7a4009b73a79ed2dc9bcc0260a59aaa32
xcm <1095429765@qq.com>
2024-07-17 22:49:05 +0800

    task 1 implement: tty output function

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,6 +6,7 @@ void _kernel_init() {
 
 void _kernel_main(void* info_table) {
     // TODO
+    tty_clear();
     tty_set_theme(VGA_COLOR_GREEN, VGA_COLOR_BLACK);
-    tty_put_str("Hello kernel world!\nThis is second line.");
+    tty_put_str("Hello kernel world!\rThis is second line.");
 }
--- kernel/tty/tty.c
+++ kernel/tty/tty.c
@@ -8,19 +8,39 @@ vga_atrributes *buffer = 0xB8000;
 
 vga_atrributes theme_color = VGA_COLOR_BLACK;
 
-uint32_t TTY_COLUMN = 0;
-uint16_t TTY_ROW = 0;
+uint32_t tty_column = 0;
+uint16_t tty_row = 0;
 
 void tty_set_theme(vga_atrributes fg, vga_atrributes bg) {
-    
+    theme_color = (bg << 4 | fg) << 8;
 }
 
 void tty_put_char(char chr) {
+    if (chr == '\r') {
+        tty_column = 0;
+    } else if (chr == '\n') {
+        tty_column = 0;
+        tty_row++;
+    } else {
+        *(buffer + tty_row * TTY_WIDTH + tty_column) = (theme_color | chr);
+        tty_column++;
+        if (tty_column >= TTY_WIDTH) {
+            tty_row++;
+            tty_column = 0;
+        }
 
+        if (tty_row >= TTY_HEIGHT) {
+            tty_scroll_up();
+            tty_row--;
+        }
+    }
 }
 
 void tty_put_str(char* str) {
-
+    while (*str != '\0') {
+        tty_put_char(*str);
+        str++;
+    }
 }
 
 void tty_scroll_up() {
@@ -28,5 +48,9 @@ void tty_scroll_up() {
 }
 
 void tty_clear() {
-
+    for (uint32_t row = 0; row < TTY_HEIGHT; ++row) {
+        for (uint32_t col = 0; col < TTY_WIDTH; ++col) {
+            *(buffer + row * TTY_WIDTH + col) = VGA_COLOR_BLACK;
+        }
+    }
 }
